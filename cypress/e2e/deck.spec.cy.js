c148f48a9f62c3d2d7f94a6ffd92cb2a8b1dec45
Joao Dias <joao.dias@ingenious.build>
2022-09-18 21:21:39 -0300

    test: Deck API tests and commands

---
+++ cypress/e2e/deck.spec.cy.js
@@ -0,0 +1,16 @@
+Cypress.config('baseUrl','http://deckofcardsapi.com/api')
+describe('Deck of Cards API', () => {
+  it('Create a deck', () => {
+    cy.createDeck().then(deck_id=>{
+      cy.shuffleDeck(deck_id);
+      cy.drawCards(deck_id,3);
+      cy.createPile(deck_id,"pile1",5)
+      cy.createPile(deck_id,"pile2",5)
+      cy.listCards(deck_id,"pile1",5)
+      cy.listCards(deck_id,"pile2",5)
+      cy.shufflePile(deck_id,"pile1")
+      cy.drawCardsFromPile(deck_id,"pile1",2)
+      cy.drawCardsFromPile(deck_id,"pile2",3)
+    })
+  })
+})
